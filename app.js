c4ea84485213192071f5d94ce51998376ac45969
Kar Kang Garence Wong <s10262458@connect.np.edu.sg>
2024-06-25 14:49:31 +0800

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
 const app = express();
 
-const port = 3000;
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', (req, res) => {
+    const user = { name: req.body.name, password: req.body.password }
+    users.push(user)
+    res.status(201).send()
+})
+
+app.listen(3000)
---
+++ dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "username",
+    password: "password",
+    server: "localhost",
+    database: "healthcare_db",
+    trustServerCertificate: true,
+    options: {
+      port: 1433,
+      connectionTimeout: 60000,
+    },
+  };
