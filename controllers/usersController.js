755f8498c2a5a9a107bd7b839922fd9d8c8edd89
Kar Kang Garence Wong <s10262458@connect.np.edu.sg>
2024-06-26 22:17:45 +0800

    Update user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,9 @@
+// usersController.js
+const sql = require("mssql");
+const bcrypt = require("bcrypt");
+const dbConfig = require("../../dbconfig")
 const User = require("../models/user");
 
-// Controller function for user signup
 async function signup(req, res, next) {
   const { username, email, password, number } = req.body;
 
@@ -8,16 +11,16 @@ async function signup(req, res, next) {
     const newUser = await User.createUser(username, email, password, number);
     res.status(201).json(newUser);
   } catch (error) {
-    console.error("Error signing up:", error.message);
+    console.error("Error signing up:", error.message, error.stack);
     res.status(500).json({ message: "Signup failed", error: error.message });
   }
 }
 
-// Controller function for user login (not implemented here)
 async function login(req, res, next) {
   res.status(501).json({ message: "Login functionality not implemented" });
 }
 
 module.exports = { signup, login };
+
 
 
--- models/user.js
+++ models/user.js
@@ -2,75 +2,67 @@ const sql = require("mssql");
 const dbConfig = require("../dbConfig");
 
 class User {
-  constructor(id, username, email, password, number) {
-    this.id = id;
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.number = number;
-  }
-
-  static async createUser(username, email, password, number) {
-    try {
-      const pool = await sql.connect(dbConfig);
-
-      const sqlQuery = `
-        INSERT INTO Users (username, email, password, number)
-        OUTPUT INSERTED.id, INSERTED.username, INSERTED.email, INSERTED.password, INSERTED.number
-        VALUES (@username, @email, @password, @number);
-      `;
-
-      const request = connection.request();
-      request.input("username", sql.VarChar, username);
-      request.input("email", sql.VarChar, email );
-      request.input("password", sql.VarChar, password);
-      request.input("number", sql.VarChar, number);
-
-      const result = await request.query(sqlQuery);
-
-      connection.close();
-      
-      return new User(
-        result.recordset[0].id,
-        result.recordset[0].username,
-        result.recordset[0].email,
-        result.recordset[0].password,
-        result.recordset[0].number
-      );
-    } catch (err) {
-      throw new Error(`Error creating user: ${err.message}`);
+    constructor(id, username, email, password, number) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.number = number;
     }
-  }
 
-  static async getUserByEmail(email) {
-    try {
-      const pool = await sql.connect(dbConfig);
+    static async createUser(username, email, password, number) {
+        const connection = await sql.connect(dbConfig);
 
-      const sqlQuery = `SELECT * FROM Users WHERE email = @Email;`;
+        const sqlQuery = `
+            INSERT INTO Users (username, email, password, number)
+            OUTPUT INSERTED.id, INSERTED.username, INSERTED.email, INSERTED.password, INSERTED.number
+            VALUES (@username, @Email, @Password, @Number)
+        `;
 
-      const request = pool.request();
-      request.input("Email", sql.VarChar, email);
+        const request = connection.request();
+        request.input("username", sql.VarChar, username);
+        request.input("email", sql.VarChar, email);
+        request.input("password", sql.VarChar, password);
+        request.input("number", sql.VarChar, number);
+        const result = await request.query(sqlQuery);
 
-      const result = await request.query(sqlQuery);
+        connection.close();
 
-      sql.close();
-
-      return result.recordset[0]
-        ? new User(
+        return new User(
             result.recordset[0].id,
             result.recordset[0].username,
             result.recordset[0].email,
             result.recordset[0].password,
             result.recordset[0].number
-          )
-        : null;
-    } catch (err) {
-      throw new Error(`Error finding user by email: ${err.message}`);
+        );
     }
-  }
+
+    static async getUserByEmail(email) {
+        const connection = await sql.connect(dbConfig);
+
+        const sqlQuery = `SELECT * FROM Users WHERE email = @Email`;
+
+        const request = connection.request();
+        request.input("email", sql.VarChar, email);
+        const result = await request.query(sqlQuery);
+
+        connection.close();
+
+        return result.recordset[0]
+            ? new User(
+                result.recordset[0].id,
+                result.recordset[0].username,
+                result.recordset[0].email,
+                result.recordset[0].password,
+                result.recordset[0].number
+            )
+            : null;
+    }
 }
 
 module.exports = User;
+
+
 
 
 
