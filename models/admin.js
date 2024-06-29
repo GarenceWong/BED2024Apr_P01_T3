4526653b8ded1140e2949f54b982219840c8bcbc
izwannp <izwnsyhrizt@gmail.com>
2024-06-29 13:54:02 +0800

    app.js

---
+++ models/admin.js
@@ -0,0 +1,42 @@
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+
+class Admin {
+    constructor(id, email, password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    static async getAdminByEmail(email) {
+        try {
+            const connection = await sql.connect(dbConfig);
+            const sqlQuery = `SELECT * FROM Users WHERE email = @Email`;
+
+            const request = connection.request();
+            request.input("Email", sql.VarChar, email);
+            const result = await request.query(sqlQuery);
+            connection.close();
+
+            if (result.recordset.length > 0) {
+                const user = result.recordset[0];
+                return new Login(user.email, user.password);
+            }
+            return null;
+        } catch (err) {
+            console.error("Error fetching user by email:", err);
+            throw err;
+        }
+    }
+
+    async validatePassword(password) {
+        try {
+            return await password === this.password;
+        } catch (err) {
+            console.error("Error validating password:", err);
+            throw err;
+        }
+    }
+}
+
+module.exports = Admin;
