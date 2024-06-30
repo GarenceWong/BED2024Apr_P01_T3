c22b20da92982ad4bfab94801762b54062e83dbf
izwnsyhrizt <izwnsyhrizt@gmail.com>
2024-06-30 21:09:04 +0800

    changes made

--- app.js
+++ app.js
@@ -40,19 +40,16 @@ app.post('/new-appointment', async (req, res) => {
   }
 });
  
-// Replace this with your database query logic to fetch appointments
 async function fetchAppointmentsFromDatabase() {
   try {
       let pool = await sql.connect(dbConfig);
       let result = await pool.request().query('SELECT * FROM Appointments');
-      return result.recordset; // Assuming appointments are retrieved as an array of objects
+      return result.recordset;
   } catch (error) {
       console.error('Error fetching appointments from database:', error.message);
       throw error;
   }
 }
-
-// Endpoint to fetch appointments
 app.get('/get-appointments', async (req, res) => {
   try {
       const appointments = await fetchAppointmentsFromDatabase();
